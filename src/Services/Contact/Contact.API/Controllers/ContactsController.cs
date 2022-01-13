73ebd8b18bec7f0dd58fe9485a8e6b6e04b4fd7a
scaltas <scaltas@gmail.com>
2022-01-13 17:24:25 +0300

    Added Contact.Api controller.

---
+++ src/Services/Contact/Contact.API/Controllers/ContactsController.cs
@@ -0,0 +1,71 @@
+﻿using Contact.API.Application;
+using Contact.API.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Contact.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ContactsController : ControllerBase
+    {
+        private readonly IContactService _contactService;
+
+        public ContactsController(IContactService contactService) =>
+            _contactService = contactService;
+
+        [HttpGet]
+        public async Task<List<ContactEntry>> Get() =>
+            await _contactService.GetAsync();
+
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<ContactEntry>> Get(string id)
+        {
+            var contact = await _contactService.GetAsync(id);
+
+            if (contact is null)
+            {
+                return NotFound();
+            }
+
+            return contact;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post(ContactEntry newContact)
+        {
+            await _contactService.CreateAsync(newContact);
+
+            return CreatedAtAction(nameof(Get), new { id = newContact.Id }, newContact);
+        }
+
+        [HttpPut("{id:length(24)}")]
+        public async Task<IActionResult> Update(string id, ContactEntry updatedContact)
+        {
+            var contact = await _contactService.GetAsync(id);
+
+            if (contact is null)
+            {
+                return NotFound();
+            }
+
+            await _contactService.UpdateAsync(id, updatedContact);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:length(24)}")]
+        public async Task<IActionResult> Delete(string id)
+        {
+            var contact = await _contactService.GetAsync(id);
+
+            if (contact is null)
+            {
+                return NotFound();
+            }
+
+            await _contactService.RemoveAsync(contact.Id);
+
+            return NoContent();
+        }
+    }
+}
